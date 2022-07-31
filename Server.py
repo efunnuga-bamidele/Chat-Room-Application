1a4855d61e96d35c4912abff7ff1c97ce83f21aa
Bamidele Efunnuga <bamideleefunnuga9@gmail.com>
2022-07-31 14:46:14 +0100

    Creating the Server

---
+++ Server.py
@@ -0,0 +1,15 @@
+import socket
+
+host_address = 'localhost'
+host_port = 8080
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.bind(host_address, host_port)
+
+sock.listen(1)
+conn, address = sock.accept()
+
+message = "Hey there is something important for you"
+conn.send(message.encode())
+
+conn.close()
